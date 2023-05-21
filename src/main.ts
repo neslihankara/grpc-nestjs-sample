bc02a73a07b011f84b8116594762de63c7fc94ef
neslihankara <neslihankara40@gmail.com>
2023-05-21 22:07:53 +0300

    implement grpc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+// import { grpcClientOptions } from './grpc-client.options';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'hero',
+        protoPath: join(__dirname, './hero/hero.proto'),
+      },
+    },
+  );
+  await app.listen();
+
+  // const app = await NestFactory.create(AppModule);
+
+  // app.connectMicroservice<MicroserviceOptions>(grpcClientOptions);
+
+  // // await app.startAllMicroservices();
+
+  // await app.listen(3000);
 }
 bootstrap();
